418b3b794859d3759dec09eebb04975d60c91d94
Chad Stabler <stablerpsn@gmail.com>
2022-04-27 13:24:41 -0500

    finished event listener for cheese

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const soda = document.getElementById('soda');
+const cheese = document.getElementById('cheese');
 
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
+});
+
+cheese.addEventListener('click', () => {
+    cheese.classList.toggle('picked');
 });
