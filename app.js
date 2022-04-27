6b1d06b3da0efaa90461ac59f168e16ea9aec512
Chad Stabler <stablerpsn@gmail.com>
2022-04-27 13:46:54 -0500

    added button for random pick

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const cheese = document.getElementById('cheese');
 const grapes = document.getElementById('grapes');
 const bread = document.getElementById('bread');
 const melon = document.getElementById('watermelon');
+const btn = document.getElementById('randomPick');
+
+function randomItem() {
+    if (Math.ceil(Math.random() * 5) === 1) {
+        return 'soda';
+    } else if (Math.ceil(Math.random() * 5) === 2) {
+        return 'cheese';
+    } else if (Math.ceil(Math.random() * 5) === 3) {
+        return 'grapes';
+    } else if (Math.ceil(Math.random() * 5) === 4) {
+        return 'bread';
+    } else return 'watermelon';
+    
+}
 
 soda.addEventListener('click', () => {
     soda.classList.toggle('picked');
@@ -22,4 +36,8 @@ bread.addEventListener('click', () => {
 
 melon.addEventListener('click', () => {
     melon.classList.toggle('picked');
+});
+
+btn.addEventListener('click', () => {
+    document.getElementById(`${randomItem()}`).classList.add('picked');
 });
